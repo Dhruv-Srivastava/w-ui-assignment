e985ea67d3a327844ba6dbc81c3baa0ee74b2d1b
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-07-27 21:36:41 +0530

    Add filter data

---
+++ src/data/index.js
@@ -0,0 +1,15 @@
+export const locations = [
+  "Pune",
+  "Goa",
+  "Rishikesh",
+  "Delhi",
+  "Kerala",
+  "Mumbai",
+  "Chennai",
+  "Agra",
+  "Hyderabad",
+  "Varanasi",
+  "Kolkata",
+];
+
+export const type = ["Signature", "Standalone"]
