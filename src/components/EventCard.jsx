c98b2cdc69133e318a66a7c197160eee0c47ed08
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-07-27 22:57:51 +0530

    Update EventCard

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,21 +1,31 @@
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+
 import LocationLogo from "../assets/location.svg";
 import DateLogo from "../assets/date.svg";
+
 import { getDate } from "../utils/get-date";
 import { capitalize } from "../utils/capitalize";
 
-export default function EventCard({ item }) {
+export default function EventCard({ item, variants }) {
+  // console.log("Hi I'm re-rendering");
   const navigate = useNavigate();
-  const dateTimeString = getDate(item.date * 1000).split(" ").slice(1).join(" ");
+  const dateTimeString = getDate(item.date * 1000)
+    .split(" ")
+    .slice(1)
+    .join(" ");
+
   return (
-    <div
+    <motion.div
       className="relative text-white w-full max-w-[400px] shadow-lg rounded-lg flex flex-col gap-3 cursor-pointer overflow-hidden"
       onClick={() => navigate(`/${item.id}`)}
+      variants={variants}
+      key={item}
     >
       <img
         src={item.image}
         alt=""
-        className="w-full h-[200px] object-cover object-center rounded-md hover:scale-105 transition-transform "
+        className="w-full h-[200px] object-cover object-center rounded-md hover:scale-105 transition-[300ms_ease_transform] lg:h-[300px]"
       />
       <div className="w-[95%] absolute bottom-2 m-0 left-[0.625rem] grid gap-6 grid-cols-[7fr_3fr] py-4 px-3 rounded-lg bg-transparent backdrop-blur-[30px]">
         <div className="text-sm font-normal">
@@ -34,6 +44,6 @@ export default function EventCard({ item }) {
           <p className="text-xl font-semibold">&#8377;{item.price}</p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
