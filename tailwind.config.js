0a7e4ba9326ca2f54286823b7f84c7391ecfad64
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-07-27 23:06:29 +0530

    Add some config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    transitionDuration: {
+      DEFAULT: "250ms",
+    },
+    transitionTimingFunction: {
+      DEFAULT: "ease-in-out",
+    },
     extend: {
-      fontFamily:{
-        inter:["Inter", "sans-serif"]
-      }
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-pattern": "url('assets/hero-pattern.svg')",
+      },
     },
   },
   plugins: [],
-}
+};
